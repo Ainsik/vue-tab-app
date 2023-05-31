b830bd5619f9620a9d89f639dac18feb42bb03a1
Ainsik <pio.andrzejczuk@gmail.com>
2023-05-31 19:57:15 +0200

    keep alive data in form

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -11,7 +11,9 @@
 			>Add Resource</base-button
 		>
 	</base-card>
-	<component :is="selectedTab"></component>
+	<keep-alive>
+		<component :is="selectedTab"></component>
+	</keep-alive>
 </template>
 
 <script>
@@ -50,11 +52,22 @@ export default {
 		setSelectedTab(tab) {
 			this.selectedTab = tab;
 		},
+		addResource(title, description, url) {
+			const newResource = {
+				id: new Date().toISOString(),
+				title: title,
+				description: description,
+				link: url,
+			};
+			this.storedResources.unshift(newResource);
+			this.selectedTab = "stored-resources";
+		},
 	},
 	components: { StoredResources, AddResource },
 	provide() {
 		return {
 			resources: this.storedResources,
+			addResource: this.addResource,
 		};
 	},
 };
