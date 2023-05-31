2d89f1d642d2638b3022f2c4d13e8d6d0e23cd83
Ainsik <pio.andrzejczuk@gmail.com>
2023-05-31 19:33:13 +0200

    change logic for resources component

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,61 @@
+<template>
+	<base-card>
+		<base-button
+			@click="setSelectedTab('stored-resources')"
+			:mode="storeResButtonMode"
+			>Stored Resources</base-button
+		>
+		<base-button
+			@click="setSelectedTab('add-resource')"
+			:mode="addResButtonMode"
+			>Add Resource</base-button
+		>
+	</base-card>
+	<component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from "./StoredResources.vue";
+import AddResource from "./AddResource.vue";
+
+export default {
+	data() {
+		return {
+			selectedTab: "stored-resources",
+			storedResources: [
+				{
+					id: "official-guide",
+					title: "Official Guide",
+					description: "The official Vue.js documentation",
+					link: "https://vuejs.org",
+				},
+				{
+					id: "google",
+					title: "Google",
+					description: "Learn to google",
+					link: "https://google.org",
+				},
+			],
+		};
+	},
+	computed: {
+		storeResButtonMode() {
+			return this.selectedTab === "stored-resources" ? null : "flat";
+		},
+		addResButtonMode() {
+			return this.selectedTab === "add-resource" ? null : "flat";
+		},
+	},
+	methods: {
+		setSelectedTab(tab) {
+			this.selectedTab = tab;
+		},
+	},
+	components: { StoredResources, AddResource },
+	provide() {
+		return {
+			resources: this.storedResources,
+		};
+	},
+};
+</script>
