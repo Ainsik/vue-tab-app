6aaabb2e65311a2bd64a221e84accc8ef14d387e
Ainsik <pio.andrzejczuk@gmail.com>
2023-05-31 18:43:47 +0200

    add component for base card

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import BasaCard from "./components/UI/BaseCard.vue";
 
 const app = createApp(App);
+
+app.component("base-card", BasaCard);
+
 app.mount("#app");
