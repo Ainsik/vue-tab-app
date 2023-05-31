346a3d3f181c31aa01ca7ba44b3cf805b74f7917
Ainsik <pio.andrzejczuk@gmail.com>
2023-05-31 20:44:56 +0200

    implement delete resources

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -62,12 +62,19 @@ export default {
 			this.storedResources.unshift(newResource);
 			this.selectedTab = "stored-resources";
 		},
+		removeResource(resId) {
+			const resIndex = this.storedResources.findIndex(
+				(res) => res.id === resId
+			);
+			this.storedResources.splice(resIndex, 1);
+		},
 	},
 	components: { StoredResources, AddResource },
 	provide() {
 		return {
 			resources: this.storedResources,
 			addResource: this.addResource,
+			deleteResource: this.removeResource,
 		};
 	},
 };
